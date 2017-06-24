ef1cde3f8ca7a6dde71708f751b715a2464f0f2d
csioa <babispasg@gmail.com>
2017-06-25 00:05:37 +0100

    Deliberate mistake to fail run

--- jenkins_python.py
+++ jenkins_python.py
@@ -3,10 +3,10 @@ def print_message(name):
     print "Hello, {}\n{}".format(name, message)
 
 
-def main():
+def main:
     global message
     message = "Welcome to this test"
     print_message("Babis")
 
 if __name__ == "__main__":
-    main
+    main()
